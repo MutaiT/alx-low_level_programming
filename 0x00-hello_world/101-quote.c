7632047156d98a18383d01f9ef6ad95267082952
MutaiT <mutaitimo56@gmail.com>
2023-08-10 12:24:18 -0700

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-
 #include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - A C program that prints given input
- * Return: 1 (done)
- */
+* main - A program that prints unix
+*
+* Return: 1 (Great)
+*/
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", 59);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
