03897c8bece02e7ba36def7181f6007611ea78f3
MutaiT <mutaitimo56@gmail.com>
2023-08-10 11:44:27 -0700

    Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A C program that prints given input
+ * Return: 1 (done)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", 59);
+return (1);
+}
