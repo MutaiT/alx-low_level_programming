e37fa765fb9aa0498f01400d5df06af8f40a6a67
MutaiT <mutaitimo56@gmail.com>
2023-08-10 08:20:12 -0700

    A script that outputs given information

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the given information followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
