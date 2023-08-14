39b459359653ace13973d18ea6ca6d427d146e0b
MutaiT <mutaitimo56@gmail.com>
2023-08-14 14:14:26 -0700

    Advanced Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all combinations of 2 digit numbers from 0-100
+ *
+ * Return: Great 0
+ */
+int main(void)
+{
+        int (i = 0);
+        while (i < 100)
+        i++;
+
+        int (j = 0);
+        while (j < 100)
+                ;
+        j++;
+
+        putchar("%02d %02d", i, j);
+        if (i != 99 || j != 99)
+                ;
+        putchar(", ");
+        return (0);
+}
