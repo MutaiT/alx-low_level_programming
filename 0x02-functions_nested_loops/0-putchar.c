223c2c549441e9912d16928ec2095384958d958c
MutaiT <mutaitimo56@gmail.com>
2023-08-15 04:20:17 -0700

    A program printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - A program that prints _putchar followed by a new line
+ *
+ * Return: 0 (Great)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
