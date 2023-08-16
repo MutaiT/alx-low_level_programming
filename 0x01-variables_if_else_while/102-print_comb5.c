ca4fa2c9d8e7db96740d91d6dabcf32acd5d8b30
MutaiT <mutaitimo56@gmail.com>
2023-08-16 09:07:01 -0700

    Print_comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-        int (i = 0);
-        while (i < 100)
-        i++;
+	int (i = 0);
+	while (i < 100)
+		i++;
 
-        int (j = 0);
-        while (j < 100)
-                ;
-        j++;
+	int (j = 0);
+	while (j < 100)
+		j++;
 
-        putchar("%02d %02d", i, j);
-        if (i != 99 || j != 99)
-                ;
-        putchar(", ");
-        return (0);
+	putchar("%02d %02d", i, j);
+	if (i != 99 || j != 99)
+		;
+
+	putchar(",");
+	return (0);
 }
