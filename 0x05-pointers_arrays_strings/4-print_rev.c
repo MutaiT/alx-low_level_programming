3f7bddf0692d4798e82f2e28b90a5e110d62380e
MutaiT <mutaitimo56@gmail.com>
2023-08-21 06:12:40 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - A function that prints a string in reverse
+ *
+ * @s: input string to be printed
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int tally = 0;
+
+	while (tally >= 0)
+	{
+		if (s[tally] == '\0')
+		{
+			break;
+		}
+		tally++;
+	}
+
+	for (tally--; tally >= 0; tally--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
