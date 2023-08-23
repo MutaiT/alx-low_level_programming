f013831823edd3746dda6ffb5ab99ddd1add98b8
MutaiT <mutaitimo56@gmail.com>
2023-08-23 08:26:23 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - A  function that changes lowercase  to uppercase
+ *
+ *@str: String with letters to be modified
+ *
+ * Return: Pointer to the string that is to be modified
+ */
+
+char *string_toupper(char *str)
+{
+	int x = 0;
+
+	while (str[x] != '\0')
+	{
+		if (str[x] >= 'a' && str[x] <= 'z')
+		{
+			str[x] = str[x] - ('a' - 'A');
+		}
+		x++;
+	}
+}
+
+	return (str);
+
+
