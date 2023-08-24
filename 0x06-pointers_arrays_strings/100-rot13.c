11a558d372296a6e29a706a0dc68943d6fef65eb
MutaiT <mutaitimo56@gmail.com>
2023-08-24 02:33:47 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,16 +9,24 @@
  */
 char *rot13(char *seq)
 {
-	char *ptr = seq;
-	char rotMap[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int x, y;
+	char rot13[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot14[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (; *ptr != '\0'; ptr++)
+	x = 0;
+	while (seq[x] != '\0')
 	{
-		if ((*ptr >= 'A' && *ptr <= 'Z') || (*ptr >= 'a' && *ptr <= 'z'))
+		y = 0;
+		while (rot13[y] != '\0')
 		{
-			*ptr = rotMap[*ptr - ((*ptr >= 'A' && *ptr <= 'Z') ? 'A' : 'a')];
+			if (seq[x] == rot13[y])
+			{
+				seq[x] = rot14[y];
+				break;
+			}
+			y++;
 		}
+		x++;
 	}
-
 	return (seq);
 }
