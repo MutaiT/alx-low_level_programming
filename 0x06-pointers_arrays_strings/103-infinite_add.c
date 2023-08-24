8588c92cf0b34ca553d31c2ea4d495a3b37f77e3
MutaiT <mutaitimo56@gmail.com>
2023-08-24 15:18:55 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,43 +1,49 @@
 #include "main.h"
+
 /**
  * infinite_add - A function that adds two numbers.
- * @n1: The first number.
- * @n2: The second number.
- * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
+ * @n1: The first number
+ *
+ * @n2: The second number
+ *
+ * @r: The buffer to store the result
+ *
+ * @size_r: The size of the buffer
  *
  * Return: Result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int x = 0;
-	int y = 0;
+	int sum = 0, x1, x2, y, z;
 
-	while (n1[i] != '\0' || n2[i] != '\0')
+	for (x1 = 0; n1[x1]; x1++)
+		;
+	for (x2 = 0; n2[x2]; x2++)
+		;
+	if (x1 > size_r || x2 > size_r)
+		return (0);
+	x1--;
+	x2--;
+	size_r--;
+	for (y = 0; y < size_r; y++, x1--, x2--)
 	{
-		int sum = atoi(n1[y]) + atoi(n2[y]) + x;
-
-		if (sum > 9)
-		{
-			x = 1;
-			sum -= 10;
-		}
-		else
-		{
-			x = 0;
-		}
-		r[y] = sum + '0';
-		y++;
-		if (y == size_r)
-		{
-			return (0);
-		}
-	}
-	if (carry == 1)
-	{
-		r[y] = '1';
-		y++;
+		if (x1 >= 0)
+			sum += n1[x1] - '0';
+		if (x2 >= 0)
+			sum += n2[x2] - '0';
+		if (x1 < 0 && x2 < 0 && sum == 0)
+			break;
+		r[y] = sum % 10 + '0';
+		sum /= 10;
 	}
 	r[y] = '\0';
+	if (x1 >= 0 || x2 >= 0 || sum)
+		return (0);
+	for (y--, z = 0; y > z; y--, z++)
+	{
+		sum = r[y];
+		r[y] = r[z];
+		r[z] = sum;
+	}
 	return (r);
 }
