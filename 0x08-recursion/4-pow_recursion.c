c23556f5e466a490eb0d0d2c74e05f19166c04d8
MutaiT <mutaitimo56@gmail.com>
2023-08-30 09:45:57 -0700

    Returns x raised to y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
- * int _pow_recursion(int x, int y) - Function that prints x raised to power of y
+ * _pow_recursion - Calculates the result of x raised to the power of y
  *
+ * @x: The base number
+ * @y: The exponent
  *
- * @x: Base
- *
- * @y: Superscript
- *
- * Return: x raised to the power of y
+ * Return: The result of x raised to the power of y
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (-1);
+	{
+		/* Negative exponent is not supported, so return -1 to indicate an error */
+		/* return -1; */
+	}
 	else if (y == 0)
-		return (1);
+	{
+		/* Any number raised to the power of 0 is 1 */
+		/* return 1; */
+	}
 	else
-		return (x * _pow_recursion(x, y - 1));
+	{
+		/* Recursively calculate x^y by multiplying x with x^(y-1) */
+		/* return x * _pow_recursion(x, y - 1); */
+	}
 }
