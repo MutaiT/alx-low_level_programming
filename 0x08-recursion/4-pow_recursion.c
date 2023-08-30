5913aca429477da8019080dfa857296872ab5f6d
MutaiT <mutaitimo56@gmail.com>
2023-08-30 10:46:07 -0700

    Returns x raised to y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,17 +12,14 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		/* Negative exponent is not supported, so return -1 to indicate an error */
-		/* return -1; */
+		return (-1);
 	}
 	else if (y == 0)
 	{
-		/* Any number raised to the power of 0 is 1 */
-		/* return 1; */
+		return (1);
 	}
 	else
 	{
-		/* Recursively calculate x^y by multiplying x with x^(y-1) */
-		/* return x * _pow_recursion(x, y - 1); */
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
