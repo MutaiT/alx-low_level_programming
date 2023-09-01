1c7fa2dd50553e8362d2fbc997059ec61643b273
MutaiT <mutaitimo56@gmail.com>
2023-09-01 13:59:56 -0700

    Advanced Task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,24 +10,32 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	/* Check if the number of arguments is not exactly 1 */
+	if (argc < 2)
 	{
+	/* If the conditiion is met, print "Error" followed by a new line */
 		printf("Error\n");
 		return (1);
 	}
 
+	/* Convert the argument provided by the user to integer "cents" */
 	int cents = atoi(argv[1]);
 
+	/* We check if the value of the variable "cents" is negative */
 	if (cents < 0)
 	{
+	/* If "cents" is negative we print 0 */
 		printf("0\n");
 		return (0);
 	}
 
+	/* We declare an array called "coins" with the coin denominations */
 	int coins[] = {25, 10, 5, 2, 1};
 	int coin_count = 0;
+	int i;
 
-	for (int i = 0; i < 5; i++)
+	/* Iterate through available coin denominations */
+	for (i = 0; i < 5; i++)
 	{
 		while (cents >= coins[i])
 		{
@@ -36,6 +44,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	/* Print the value of "coin_count" */
 	printf("%d\n", coin_count);
 	return (0);
 }
