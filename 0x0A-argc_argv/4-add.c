e7ced995b19d95e901b23f0841d80ad8ca5f40c6
MutaiT <mutaitimo56@gmail.com>
2023-09-01 08:59:09 -0700

    Adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - Adds positive numbers
+ *
+ * @argc: Number of arguments
+ * @argv: Argument vector
+ *
+ * Return: 1 or 0 (Success)
+ */
+int main(int argc, char *argv[])
+
+	int sum;
+
+	if (argc < 2)
+	{
+		printf("0\n"); /* No numbers provided, so print 0 */
+		return (0); /* Success */
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+	/* Convert the argument to an integer */
+		int num = atoi(argv[i]);
+
+	/* Check if the conversion was successful */
+		if (num == 0 && argv[i][0] != '0')
+		{
+			printf("Error\n");
+			return (1); /* Conversion error */
+		}
+
+		if (num > 0)
+		{
+			sum += num; /* Add the positive number to the sum */
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0); /* Return success */
+}
+
