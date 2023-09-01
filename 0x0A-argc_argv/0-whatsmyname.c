53b5002d5a410920be88158f35ea8a759e216f95
MutaiT <mutaitimo56@gmail.com>
2023-09-01 05:45:14 -0700

    Prints programme name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -13,12 +12,7 @@
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	{
-		printf("Program name: %s\n", argv[0]);
-	}
-	else
-	{
-		printf("Unable to determine program name.\n");
-	}
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
