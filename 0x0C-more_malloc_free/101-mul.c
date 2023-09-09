15cc9a411371c599cca0a4e080a72d976e25f2ff
MutaiT <mutaitimo56@gmail.com>
2023-09-09 10:15:18 -0700

    Advanced Task 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,60 +1,26 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _calloc - calloc function
+ * lenstr - Gets the length of a string.
  *
- * @nmemb: Number of elements
- * @size: Size of each element
+ * @s: The string to calculate the length of.
  *
- * Return: Pointer to the memory space
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	unsigned int x;
-	char *y;
-
-	if (size == 0 || nmemb == 0)
-		return (NULL);
-	y = malloc(nmemb * size);
-	if (y == NULL)
-		return (NULL);
-	for (x = 0; x < nmemb * size; ++x)
-		y[x] = 0;
-	return (y);
-}
-
-/**
- * lenstr - Gets the length of a string
- *
- * @s - The string
- *
- * Return: Length of the string
+ * Return: Length of the string.
  */
 int lenstr(char *s)
 {
 	int x;
 
-	for (x = 0; s[x]; ++x)
-		;
+	for x = 0; s[x];
+	++x;
+
 	return (x);
 }
 
 /**
- * _prints - Prints using putchar function
- *
- * @str: String to be printed
- *
- * Return: No value
- */
-void _puts(char *str)
-{
-	while (*str)
-		_putchar(*(str++));
-}
-
-/**
- * _isdigit
+ * _strdigit - A program that checks for a digit from 0 through 9
  *
  * @s1: String
  *
@@ -75,81 +41,6 @@ int _strdigit(char *s1)
 		++s1;
 	}
 	return (1);
-}
-
-/**
- * reverse_str - Reverse string
- *
- * @First: First index
- * @Last: Last index
- * @str: String to be reversed
- *
- * Return: No value
- */
-void rev_num_str(int First, int Last, char *str)
-{
-	int x, y;
-	char z;
-
-	while (str[Last] == 0 && Last != First)
-		--Last;
-	for (x = First, y = Last; x <= y; ++x, --y)
-	{
-		z = str[x] + '0';
-		str[x] = str[y] + '0';
-		str[y] = z;
-	}
-}
-
-/**
- * _multiply - Multiply a long string
- *
- * @s1: First string
- * @s2: Second string
- *
- * Return: Pointer to the answer
- */
-char *strmul(char *s1, char *s2)
-{
-	int v1, v2, x, y, c, d, n = 0;
-	char *r;
-	char mul, mul_, sum, sum_;
-
-	if (*s1 == '-')
-	{
-		n ^= 1;
-		++s1;
-	}
-	if (*s2 == '-')
-	{
-		n ^= 1;
-		++s2;
-	}
-	v1 = lenstr(s1);
-	v2 = lenstr(s2);
-	r = _calloc(v1 + v2 + 1 + n, sizeof(char));
-	if (r == NULL)
-		return (NULL);
-	if (n)
-		r[0] = '-';
-	for (x = v2 - 1, c = n; x >= 0; --x, ++c)
-	{
-		mul_ = 0;
-		sum_ = 0;
-		for (y = v1 - 1, d = c; y >= 0; --y, ++d)
-		{
-			mul = (s1[y] - '0') * (s2[x] - '0') + mul_;
-			mul_ = mul / 10;
-			mul %= 10;
-			sum = r[d] + mul + sum_;
-			sum_ = sum / 10;
-			sum %= 10;
-			r[d] = sum;
-		}
-		r[d] = sum_ + mul_;
-	}
-	rev_num_str(n, d, r);
-	return (r);
 }
 
 /**
