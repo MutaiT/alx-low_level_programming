b1bd7a015816bb9947541612ce5d03ed13011b14
MutaiT <mutaitimo56@gmail.com>
2023-09-23 10:04:00 -0700

    Returns number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns number of elements in a linked list_t list
+ * @h: Address of the first element
+ *
+ * Return: Elements' number
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (i)
+	{
+		n++;
+		i = i->next;
+	}
+	return (n);
+}
