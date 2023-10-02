762bd9058f0a3c43fd809a631bb1280c2b0b2750
MutaiT <mutaitimo56@gmail.com>
2023-10-02 10:12:34 -0700

    Cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,33 +1,74 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * append_text_to_file - Appends text at the end of a file
+ * error_file - Checks if files can be opened.
  *
- * @filename: Filename
- * @text_content: Added content
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: Argument vector.
  *
- * Return: 1 or -1
+ * Return: No return.
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	int f;
-	int n;
-	int wr;
-
-	if (!filename)
-		return (-1);
-	f = open(filename, O_WRONLY | O_APPEND);
-	if (f == -1)
-		return (-1);
-	if (text_content)
+	if (file_from == -1)
 	{
-		for (n = 0; text_content[n]; n++)
-			;
-		wr = write(f, text_content, n);
-		if (wr == -1)
-			return (-1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	close(f);
-	return (1);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - Check the code for Holberton School students.
+ *
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nc, n;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+	nc = 1024;
+	while (nc == 1024)
+	{
+		nc = read(file_from, buf, 1024);
+		if (nc == -1)
+			error_file(-1, 0, argv);
+		n = write(file_to, buf, nc);
+		if (n == -1)
+			error_file(0, -1, argv);
+	}
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
