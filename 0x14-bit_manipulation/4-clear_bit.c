44e17bf20ebfa35342d1d63d84ffbbd0331c0c47
MutaiT <mutaitimo56@gmail.com>
2023-10-26 08:34:10 -0700

    Task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,7 +3,7 @@
 /**
  * clear_bit - Sets the value of a bit at a given index to 0
  *
- * @n: A pointer to the bit
+ * @n: Pointer to the bit
  * @index: The index to set the value at - indices start at 0
  *
  * Return: -1 or 1
